113d6b7f0a1e7d86f2d99570beb247906b6e4602
Matt Kerlogue <matt.kerlogue@me.com>
2022-11-04 17:57:15 +0000

    find CSS files

--- R/deckhand.R
+++ R/deckhand.R
@@ -19,6 +19,40 @@ deckhand <- function(css = NULL, page_size = NULL, ...) {
 
   if (!is.null(css)) {
     css <- c(core_css, css)
+  } else {
+
+    # try and find a _deckhand.css file
+    if (file.exists(file.path(getwd(), "_deckhand.css"))) {
+      dh_css <- file.path(getwd(), "_deckhand.css")
+    } else if (file.exists(file.path(getwd(), "css", "_deckhand.css"))) {
+      dh_css <- file.path(getwd(), "css", "_deckhand.css")
+    } else if (file.exists(file.path(here::here("_deckhand.css")))) {
+      dh_css <- file.path(here::here("_deckhand.css"))
+    } else {
+
+      # find a deckhand yaml file and convert it
+      if (file.exists(file.path(getwd(), "_deckhand.yml"))) {
+        config_yml <- file.path(getwd(), "_deckhand.yml")
+        dh_dir <- getwd()
+        warn_msg <- config_yml
+      } else if (file.exists(file.path(here::here("_deckhand.yml")))) {
+        config_yml <- file.path(here::here("_deckhand.yml"))
+        dh_dir <- here::here()
+        warn_msg <- config_yml
+      } else {
+        config_yml <- NULL
+        dh_dir <- getwd()
+        warn_msg <- "default settings"
+      }
+
+      warning("No _deckhand.css file located, generating from ")
+      dh_css <- deckhand_config_to_css(config_yml = config_yml, dh_dir = dh_dir)
+      message("Deckhand CSS saved as: ", dh_css)
+
+    }
+
+    css <- c(core_css, dh_css)
+
   }
 
   pagedown::html_paged(
